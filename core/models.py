5cbfe6c711d3299f3ad7cb075799cdd1a71a4f1a
Vasyl Cherkes <cherkesva@gmail.com>
2025-11-14 00:21:22 +0200

    create models for base app core

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    image = models.ImageField(upload_to="reviews/", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.author_name
+
+
+class Certificate(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="certificates/")
+
+    def __str__(self):
+        return self.title or "Certificate"
+
+
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=20)
+    telegram_url = models.URLField(blank=True)
+    instagram_url = models.URLField(blank=True)
+    address = models.CharField(max_length=255)
+    map_embed_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.phone
+
+
