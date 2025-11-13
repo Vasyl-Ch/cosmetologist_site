cb301f83a3fc491de19ffe9607a61a52827a6b12
Vasyl Cherkes <cherkesva@gmail.com>
2025-11-14 00:34:40 +0200

    create models for procedures app

--- procedures/models.py
+++ procedures/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class ProcedureType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="procedure_types/")
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Procedure(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="procedures/")
+    duration_minutes = models.IntegerField(blank=True, null=True)
+    price = models.DecimalField(blank=True, null=True, max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(blank=True, null=True, max_digits=10, decimal_places=2)
+    slug = models.SlugField(unique=True)
+    type = models.ForeignKey(ProcedureType, on_delete=models.CASCADE, related_name="procedures")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
