02f2da32e03bf38827776986105f9f764aebad12
Vasyl Cherkes <cherkesva@gmail.com>
2025-11-18 21:39:02 +0200

    staticfiles managing and restruct settings for dev/prod

--- manage.py
+++ manage.py
@@ -3,12 +3,16 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
-        "cosmetologist_site.settings",
+        "cosmetologist_site.settings.dev",
     )
     try:
         from django.core.management import execute_from_command_line
