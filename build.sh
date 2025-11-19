ffb32e65c2880c6fd79ccef39732ba4480a9c9a4
Vasyl Cherkes <cherkesva@gmail.com>
2025-11-19 03:21:04 +0200

    fix build file

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ python manage.py collectstatic --noinput
 #Apply any outstanding database migrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
